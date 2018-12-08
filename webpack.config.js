b49ff836dbd46aa9634675aeda74f20eadfbdd7f
entipe <gauthier.depaoli@gmail.com>
2018-12-08 12:04:23 +0100

    upd config

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,23 @@ let config = {
 	      }
 	    }
 	  ]
+	},
+	mode: 'production',
+	devtool : 'none',
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendors: {
+					priority: -10,
+					test: /[\\/]node_modules[\\/]/
+				}
+			},
+
+			chunks: 'async',
+			minChunks: 1,
+			minSize: 30000,
+			name: false
+		}
 	}
 }
 
