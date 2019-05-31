49792f41d7961260f26fad06cdda5a9227c31f39
entipe <gauthier.depaoli@gmail.com>
2019-05-31 23:31:49 +0200

    Show more recent agenda, then older

---
+++ src/js/date.js
@@ -0,0 +1,36 @@
+define(function() {
+  const ITEM_CLASS = '.js-item-date';
+  const ITEM_CONTAINER_CLASS = '.js-item-date-container';
+  const CURRENT_DATE = new Date();
+  const $itemContainer = document.querySelector(ITEM_CONTAINER_CLASS);
+  const MAX_ITEM = 10;
+
+
+  let $itemList;
+
+  const init = function () {
+    $itemList = getItemList();
+
+    $itemList.forEach(item => {
+      let date = new Date(parseInt(item.getAttribute('data-date')) * 1000);
+      if (date < CURRENT_DATE) {
+        $itemContainer.removeChild(item);
+      }
+    });
+
+    $itemList = getItemList();
+    if ($itemList.length >= MAX_ITEM) {
+      for (let i = MAX_ITEM; i < $itemList.length; i++) {
+        $itemList[i].classList.remove('is-active');
+      }
+    } else {
+      document.querySelector('.js-view-more-button-container').classList.add('hidden');
+    }
+  }
+
+  const getItemList = function () {
+    return document.querySelectorAll(ITEM_CLASS);
+  }
+
+  init();
+});
